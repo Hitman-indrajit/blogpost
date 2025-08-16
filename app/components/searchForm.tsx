6925e308a31aa9b7cbd7489bef9c97a2f25c9aa1
Suraj Mongad <suraj_mongad@persistent.com>
2025-08-16 23:15:00 +0100

    stable design

--- app/components/searchForm.tsx
+++ app/components/searchForm.tsx
@@ -4,19 +4,25 @@ import SearchFormReset from "./SearchFormReset";
 
 const SearchForm = ({ query }: { query?: string }) => {
   return (
-    <Form action="/" scroll={false} className="search-form">
-      <input
-        type="text"
-        name="query"
-        defaultValue={query}
-        placeholder="Search..."
-        className="search-input"
-      />
-      {query && <SearchFormReset />}
-      <button type="submit" className="search-button">
-        S
-      </button>
-    </Form>
+    <div className="flex items-center justify-center">
+      <Form
+        action="/"
+        scroll={false}
+        className="form-default flex items-center gap-2"
+      >
+        <input
+          type="text"
+          name="query"
+          defaultValue={query}
+          placeholder="Search..."
+          className="form-input"
+        />
+        {query && <SearchFormReset />}
+        <button type="submit" className="form-submit">
+          S
+        </button>
+      </Form>
+    </div>
   );
 };
 
